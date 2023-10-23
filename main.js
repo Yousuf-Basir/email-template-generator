93c9f58b192a6a321e15008f85f47447aa67ff7f
yousuf <gourabxz@gmail.com>
2023-10-23 11:24:31 +0600

    add dotted background

--- main.js
+++ main.js
@@ -1,5 +1,12 @@
 const copyButton = document.getElementById('copy-button');
 const emailSignature = document.getElementById('email-signature');
+const editButton = document.getElementById('edit-button');
+
+// user data from email signature body
+const userName = document.getElementById('user-name');
+const userDesignation = document.getElementById('user-designation');
+const userPhoneNumber = document.getElementById('user-phone-number');
+const userEmail = document.getElementById('user-email');
 
 
 const copyRichText = async (innerHTML) => {
@@ -13,7 +20,7 @@ copyButton.addEventListener('click', () => {
     // Copy the email signature to the clipboard as rich text
     navigator.clipboard.writeText(emailSignature.innerText).then(() => {
         // Show a success message
-        copyButton.innerText = 'Copied!';
+        copyButton.innerText = 'Copied';
         setTimeout(() => {
             copyButton.innerText = 'Copy';
         }, 2000);
@@ -25,3 +32,29 @@ copyButton.addEventListener('click', () => {
         }, 2000);
     });
 });
+
+// get user data values from email signature body
+const getUserData = () => {
+    const userData = {
+        name: userName.innerText,
+        designation: userDesignation.innerText,
+        mobileNumber: userPhoneNumber.innerText,
+        email: userEmail.innerText
+    }
+    return userData;
+}
+
+// navigate to edit page
+editButton.addEventListener('click', () => {
+    const userData = getUserData();
+    const userDataString = JSON.stringify(userData);
+    window.location.href = `edit.html?user=${userDataString}`;
+});
+
+// get user data from url
+const getUserDataFromUrl = () => {
+    const urlParams = new URLSearchParams(window.location.search);
+    const userDataString = urlParams.get('user');
+    const userData = JSON.parse(userDataString);
+    return userData;
+}
